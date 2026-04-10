f5778c583af531ae69ba4e19f42725ea40ace155
ymanish1475-del <ymanish1475@gmail.com>
2026-04-10 11:12:06 +0530

    mul string num

---
+++ String/mulNumInString.java
@@ -0,0 +1,18 @@
+public class mulNumInString {
+    public static void main(String[] args) {
+        String a="12";
+        String b="5";
+        // String mul="";
+        // String s = "123";
+        int num = Integer.parseInt(a);
+        int mum=Integer.parseInt(b);
+        int mult=num*mum;
+        // int num = 100;
+        String mul = String.valueOf(mult); // "100"
+        System.out.println(mul);
+
+
+
+    }
+    
+}
