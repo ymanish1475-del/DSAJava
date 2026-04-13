a230a6f61ac087962117468596ee1a51df367f55
ymanish1475-del <ymanish1475@gmail.com>
2026-04-13 10:56:50 +0530

    count zero

--- Recursion/CountOfZero.java
+++ Recursion/CountOfZero.java
@@ -1,5 +1,21 @@
 public class CountOfZero {
+    static int countzero(int n){
+        if(n==0){
+            return 0;
+        }
+        int rem=n%10;
+        if(rem==0){
+            return 1+n;
+        }
+        // return n+n%10;
+        
 
-    
+    }
+
+    public static void main(String[] args) {
+        int n= 567080;
+        System.out.println(countzero( n));
+
+    }
     
 }
