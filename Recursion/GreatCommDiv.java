9beedb0c72d4af4e4255715201ccf026de834e2a
ymanish1475-del <ymanish1475@gmail.com>
2026-04-13 10:38:06 +0530

    gcd

---
+++ Recursion/GreatCommDiv.java
@@ -0,0 +1,13 @@
+public class GreatCommDiv {
+
+    static int gcd(int a,int b){
+        
+    }
+
+    public static void main(String[] args) {
+        int a=12;
+        int b=18;
+        System.out.println(gcd(a,b));
+    }
+    
+}
