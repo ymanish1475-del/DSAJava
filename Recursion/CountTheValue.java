d664d33dc80122ede6c92d4787dabdc7d0c06961
ymanish1475-del <ymanish1475@gmail.com>
2026-04-13 12:32:56 +0530

    count the freq in arr of a num

---
+++ Recursion/CountTheValue.java
@@ -0,0 +1,17 @@
+public class CountTheValue {
+    static int counter(int arr[],int a,int i){
+        if(i==arr.length)return 0;
+        int count = (arr[i]==a)? 1:0;
+        return count + counter(arr,i+1,a);
+
+    }
+
+    public static void main(String[] args) {
+        int arr[]={4,6,4,6,8,4,224,46,75};
+        int a=4;
+        int i =0;
+        System.out.println(counter(arr,a,i));
+
+    }
+    
+}
