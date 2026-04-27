0c54d557d698e5b4b24b7b4116d61a5017b5f21a
ymanish1475-del <ymanish1475@gmail.com>
2026-04-27 18:18:53 +0530

    backtraking

---
+++ Backtraking/new1.java
@@ -0,0 +1,18 @@
+// package Backtraking;
+
+public class new1 {
+
+    static int bt(int n ,int[]){
+
+    }
+    public static void main(String[] args){
+        int []=[1,2,1];
+        int k=2;
+        
+
+
+
+
+    }
+    
+}
