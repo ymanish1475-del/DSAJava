ddf9d8dbb71d5f50cef9049f47585fc1b28f0088
ymanish1475-del <ymanish1475@gmail.com>
2026-05-26 15:39:42 +0530

    linked list

--- LinkedList/new.java
+++ LinkedList/new.java
@@ -1,29 +1,29 @@
 // package LinkedList;
 
-class LinkedList{
-    Node head;
-    void insert(int data ){
-        Node newNode= new Node(data);
-        if(head == null){
-            head=newNode;
-            return;
-        }
-        Node temp =head;
-        while(temp.next !=null){
-            temp= temp.next;
-        }
-        temp.next= newNode;
-    }
+// class LinkedList{
+//     Node head;
+//     void insert(int data ){
+//         Node newNode= new Node(data);
+//         if(head == null){
+//             head=newNode;
+//             return;
+//         }
+//         Node temp =head;
+//         while(temp.next !=null){
+//             temp= temp.next;
+//         }
+//         temp.next= newNode;
+//     }
 
-    void print(){
-        Node.temp=head;
-    }
-}
-public class new {
+//     void print(){
+//         Node.temp=head;
+//     }
+// }
+// public class new {
 
 
     
-    public static void main (String [] args){
+//     public static void main (String [] args){
 
-    }
-}
+//     }
+// }
