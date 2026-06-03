aeb8edea28e773adca9158e6d9086ff09335637d
ymanish1475-del <ymanish1475@gmail.com>
2026-06-03 19:12:06 +0530

    stack

---
+++ LinkedList/delNode.java
@@ -0,0 +1,5 @@
+public class delNode {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Stack/newOne.java
@@ -0,0 +1,24 @@
+// package Stack;
+import java.util.Stack;
+public class newOne {
+
+    public static void main(String [] args){
+         
+    Stack <Integer> st = new Stack<>();
+    // st.push(10);
+    // st.push(20);
+    // st.push(30);
+    // st.push(40);
+    
+
+    // System.out.println(st);
+    // System.out.println();
+    Stack <Character>
+
+    String str="hellooooo";
+        for(char ch : str.toCharArray()){
+            st.push(ch);
+        }
+    }
+   
+}
