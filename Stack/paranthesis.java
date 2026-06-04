84ce1f86426173d2f58f90e5394a87fd0915aff3
ymanish1475-del <ymanish1475@gmail.com>
2026-06-04 21:30:58 +0530

    stack

---
+++ Stack/paranthesis.java
@@ -0,0 +1,11 @@
+public class paranthesis {
+    
+    static boolean inValid
+
+    public static void main(String[] args) {
+
+
+        String str="(){}[]";
+        System.out.println(inValid(str));
+    }
+}
